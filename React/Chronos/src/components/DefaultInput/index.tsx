2cef07944ddb0ff694f8982fe21bbc49bf759edd
Dhisting1 <lcs.lima0708@gmail.com>
2025-05-12 13:42:19 -0300

    estilizacao do component input

--- React/Chronos/src/components/DefaultInput/index.tsx
+++ React/Chronos/src/components/DefaultInput/index.tsx
@@ -1,14 +1,21 @@
 import type React from "react";
+import styles from "./styles.module.css";
 
 type DefaultInputProps = {
   id: string;
+  labelText: string;
 } & React.ComponentProps<"input">;
 
-export function DefaultInput({ id, type }: DefaultInputProps) {
+export function DefaultInput({
+  id,
+  labelText,
+  type,
+  ...rest
+}: DefaultInputProps) {
   return (
     <>
-      <label htmlFor={id}>Task</label>
-      <input type={type} name="" id={id} />
+      <label htmlFor={id}>{labelText}</label>
+      <input className={styles.input} type={type} name="" id={id} {...rest} />
     </>
   );
 }
