d5b12bc91f6cfb285b8d444f53b28949243f5747
Dhisting1 <lcs.lima0708@gmail.com>
2025-05-14 19:14:59 -0300

    refatorando codigo + criacao das paginas not found e about

--- React/Chronos/src/App.tsx
+++ React/Chronos/src/App.tsx
@@ -1,51 +1,8 @@
-import { PlayCircleIcon } from "lucide-react";
-import { Container } from "./components/ContainerComponent";
-import { CountDown } from "./components/CountDown";
-import { Cycles } from "./components/Cycles";
-import { DefaultButton } from "./components/DefaultButton";
-import { DefaultInput } from "./components/DefaultInput";
-import { Logo } from "./components/Logo";
-import { Menu } from "./components/Menu";
 import "./styles/global.css";
 import "./styles/theme.css";
-import { Footer } from "./components/Footer";
+
+import { Home } from "./pages/Home";
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-      <Container>
-        <Menu />
-      </Container>
-      <Container>
-        <CountDown />
-      </Container>
-      <Container>
-        <form className="form" action="">
-          <div className="formRow">
-            <DefaultInput
-              id="meuInput"
-              type="text"
-              labelText="Task"
-              placeholder="Digite sua task"
-            />
-          </div>
-          <div className="formRow">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="formRow">
-            <Cycles />
-          </div>
-          <div className="formRow">
-            <DefaultButton icon={<PlayCircleIcon />} color="green" />
-          </div>
-        </form>
-      </Container>
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
