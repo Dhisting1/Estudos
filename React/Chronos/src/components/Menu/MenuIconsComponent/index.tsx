b0bbf7fdd02970e8bc41d3214f3b60efdca3fb1e
Dhisting1 <lcs.lima0708@gmail.com>
2025-05-14 16:35:29 -0300

    criado a funcionalidade de tema dark e light

--- React/Chronos/src/components/Menu/MenuIconsComponent/index.tsx
+++ React/Chronos/src/components/Menu/MenuIconsComponent/index.tsx
@@ -1,8 +1,29 @@
 type LinksProps = {
   children: React.ReactNode;
   classe?: string;
+  ariaLabel?: string;
+  title?: string;
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement>) => void;
+  href?: string;
 };
 
-export function Links({ children, classe }: LinksProps) {
-  return <a className={classe}>{children}</a>;
+export function Links({
+  children,
+  ariaLabel,
+  title,
+  classe,
+  onClick,
+  href,
+}: LinksProps) {
+  return (
+    <a
+      className={classe}
+      aria-label={ariaLabel}
+      title={title}
+      href={href}
+      onClick={onClick}
+    >
+      {children}
+    </a>
+  );
 }
--- React/Chronos/src/components/Menu/index.tsx
+++ React/Chronos/src/components/Menu/index.tsx
@@ -1,19 +1,53 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
 import styles from "./styles.module.css";
 import { Links } from "./MenuIconsComponent";
+import { useEffect, useState } from "react";
+
+type AvailableThemes = "dark" | "light";
+
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>("dark");
+  // Função para alternar entre os temas
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent> // Evitar o comportamento padrão do link
+  ) {
+    event.preventDefault(); // Previne o comportamento padrão do link
+
+    // Alterna entre os temas. Se o tema atual for "dark", muda para "light" e vice-versa
+    // O estado é atualizado com a função setTheme
+    setTheme((prevTheme) => {
+      const nextTheme = prevTheme === "dark" ? "light" : "dark";
+      return nextTheme;
+    });
+  }
+  // Efeito colateral para aplicar o tema no elemento HTML. O useEffect é chamado sempre que o tema muda
+  // Isso garante que o tema correto seja aplicado quando o componente é montado
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme); // Pega o elemento HTML e define o atributo "data-theme" com o valor do tema atual
+  }, [theme]);
+
   return (
     <nav className={styles.menu}>
-      <Links classe={styles.menuLink}>
+      <Links classe={styles.menuLink} ariaLabel="Home" title="Home">
         <HouseIcon />
       </Links>
-      <Links classe={styles.menuLink}>
+      <Links classe={styles.menuLink} ariaLabel="Histórico" title="Histórico">
         <HistoryIcon />
       </Links>
-      <Links classe={styles.menuLink}>
+      <Links
+        classe={styles.menuLink}
+        ariaLabel="Configurações"
+        title="Configurações"
+      >
         <SettingsIcon />
       </Links>
-      <Links classe={styles.menuLink}>
+      <Links
+        classe={styles.menuLink}
+        ariaLabel="Tema"
+        title="Tema"
+        onClick={handleThemeChange}
+        href="#"
+      >
         <SunIcon />
       </Links>
     </nav>
