0f14af7450b9aba3959cd33e5c817c3e176cd379
Dhisting1 <lcs.lima0708@gmail.com>
2025-05-14 16:49:30 -0300

    adicionando o theme ao localStorage e trocando os icones sol e lua

--- React/Chronos/src/components/Menu/index.tsx
+++ React/Chronos/src/components/Menu/index.tsx
@@ -1,4 +1,10 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from "lucide-react";
 import styles from "./styles.module.css";
 import { Links } from "./MenuIconsComponent";
 import { useEffect, useState } from "react";
@@ -6,24 +12,33 @@ import { useEffect, useState } from "react";
 type AvailableThemes = "dark" | "light";
 
 export function Menu() {
-  const [theme, setTheme] = useState<AvailableThemes>("dark");
-  // Função para alternar entre os temas
-  function handleThemeChange(
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem("theme") as AvailableThemes) || "dark"; // Pega o tema armazenado no localStorage
+
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />, // Ícone do tema claro
+    light: <MoonIcon />, // Ícone do tema escuro
+  };
+
+  function handleThemeChange( // Função para alternar entre os temas
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent> // Evitar o comportamento padrão do link
   ) {
     event.preventDefault(); // Previne o comportamento padrão do link
 
-    // Alterna entre os temas. Se o tema atual for "dark", muda para "light" e vice-versa
-    // O estado é atualizado com a função setTheme
     setTheme((prevTheme) => {
+      // Alterna entre os temas. Se o tema atual for "dark", muda para "light" e vice-versa. O estado é atualizado com a função setTheme
       const nextTheme = prevTheme === "dark" ? "light" : "dark";
       return nextTheme;
     });
   }
-  // Efeito colateral para aplicar o tema no elemento HTML. O useEffect é chamado sempre que o tema muda
-  // Isso garante que o tema correto seja aplicado quando o componente é montado
   useEffect(() => {
+    // Efeito colateral para aplicar o tema no elemento HTML. O useEffect é chamado sempre que o tema muda. Isso garante que o tema correto seja aplicado quando o componente é montado
     document.documentElement.setAttribute("data-theme", theme); // Pega o elemento HTML e define o atributo "data-theme" com o valor do tema atual
+    localStorage.setItem("theme", theme); // Armazena o tema atual no localStorage
   }, [theme]);
 
   return (
@@ -48,7 +63,7 @@ export function Menu() {
         onClick={handleThemeChange}
         href="#"
       >
-        <SunIcon />
+        {nextThemeIcon[theme]} {/* Renderiza o ícone do tema atual */}
       </Links>
     </nav>
   );
