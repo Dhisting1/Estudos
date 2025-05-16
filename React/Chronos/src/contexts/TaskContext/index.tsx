3139f737efdb094176bf3b8728baea88481c4f63
Dhisting1 <lcs.lima0708@gmail.com>
2025-05-16 13:55:21 -0300

    exercicio de fixação do conteudo

---
+++ React/Chronos/src/contexts/TaskContext/index.tsx
@@ -0,0 +1,21 @@
+import { createContext } from "react";
+import type { TaskStateModel } from "../../models/TaskStateModel";
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: "00:00",
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
+
+type TaskContextType = {};
+
+export const TaskContext = createContext({
+  chave: 123,
+});
